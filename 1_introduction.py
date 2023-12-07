72344db72a2f5caab57f67bc4b920a6164c223ba
Ali Niaz <alik990487@gmail.com>
2023-12-08 02:32:28 +0500

    introduction

---
+++ 1_introduction.py
@@ -0,0 +1,26 @@
+"""
+
+    QtCore --> Non-GUI functionalities for working with file and directories.
+
+    QtGui --> Graphical user interface components
+
+    QtMultimedia --> Classes for low-level multimedia programming
+
+    QtNetwork --> Classes for network programming
+
+    QtOpenGL --> OpenGL support classes
+
+    QtScript --> Classes for evaluating Qt Scripts
+
+    QtSql --> Classes for database integration using SQL
+
+    QtSvg --> Classes for displaying the contents of SVG files
+
+    QtWebKit --> Classes for rendering and editing HTML
+
+    QtXml --> Classes for handling XML
+
+    QtWidgets --> Classes for creating classic desktop-style UIs
+
+    QtDesigner --> Classes for extending Qt Designer
+"""
