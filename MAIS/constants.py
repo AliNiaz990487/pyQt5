2e88ebfbc3409e7d64a2bbd864db08ec16114ca0
AliNiaz990487 <alik990487@gmail.com>
2024-01-12 21:05:48 +0500

    added more constants

--- MAIS/constants.py
+++ MAIS/constants.py
@@ -1,9 +1,19 @@
 import pathlib
 
-
+# resources paths
 UI = pathlib.Path(__file__).parent/"ui/mais.ui"
 PLACEHOLDER_IMAGE = pathlib.Path(__file__).parent/"assets/images/placeholder.png"
+
+# style sheets
 VISIBLE_INDICATOR = "font-size: 12px; color: rgba(255, 0, 0, 255)"
 INVISIBLE_INDICATOR = "font-size: 12px; color: rgba(255, 0, 0, 0)"
 GRAYED = "color: rgb(119, 118, 123);"
 BLACKED = "color: rgb(0, 0, 0);"
+
+# tasks indexes
+ALIGN = 0
+COMPRESS = 1
+STACK = 2  
+
+# cached
+CACHED = pathlib.Path(__file__).parent/"cached"
